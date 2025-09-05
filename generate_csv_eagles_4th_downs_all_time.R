033c7eb9e7646535ff43c168e0f09877badf2aec
Brian Mapes <1937179+bjmapes@users.noreply.github.com>
2025-09-05 10:23:57 -0400

    hardcoding 2025 as end date for now. It's just easier.

--- generate_csv_eagles_4th_downs_all_time.R
+++ generate_csv_eagles_4th_downs_all_time.R
@@ -9,9 +9,7 @@ output_path2 <- "assets/data/eagles_4th_downs_all_time.csv"
 
 
 # Load play-by-play data for all seasons
-# Load play-by-play data for all seasons
-latest <- nflfastR::most_recent_season()
-pbp_all <- load_pbp(1999:latest)
+pbp_all <- load_pbp(1999:2025)
 
 # Filter for Eagles 4th down plays
 eagles_4th_all <- pbp_all %>%
